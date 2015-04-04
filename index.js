34959681b3d7c22910315936d130e9d107090e65
Timo Behrmann <timo.behrmann@gmail.com>
2015-04-04 22:29:16 +0200

    improved

--- index.js
+++ index.js
@@ -8,7 +8,32 @@ var p = new Push(config.pushover)
 var follow = config.follow
 var params = {follow: follow.join(',')}
 
+var cleanup = function (text) {
+  if (typeof text === 'string') {
+    return text.replace(/http:\/\/[^\s#]*/g, '').replace(/#GoalFlash/g, '').replace(/^Correction: /g, 'Korrektur: ').replace(/[\s]{2,50}/g, ' ')
+  }
+  return text
+}
+
+var post = function (text) {
+  if (!text || config.post !== true) {
+    return
+  }
+
+  client.post('statuses/update', {status: text}, function (error, tweet, response) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Tweet posted: ' + text)
+    }
+  })
+}
+
 var notify = function (text) {
+  if (!text || config.pust !== true) {
+    return
+  }
+
   p.send({ message: text, title: 'Bundesliga'}, function (err, result) {
     if (err) {
       throw err
@@ -19,16 +44,16 @@ var notify = function (text) {
 }
 
 client.stream('statuses/filter', params, function (stream) {
-  notify('Pushover service running')
+  post(config.message)
 
   stream.on('data', function (tweet) {
     if (!tweet) {
       return
     } else if (_.contains(follow, tweet.user.id)) {
       console.log('Sending', tweet.user.name, tweet.user.id, tweet.text)
-      notify(tweet.text)
-    } else {
-      console.log('Skipping', tweet.user.name, tweet.user.id, tweet.text)
+      var message = cleanup(tweet.text)
+      notify(message)
+      post(message)
     }
   })
 
