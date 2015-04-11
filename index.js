cb828d5254c9f39628feec8d9b3b4049c72117eb
Timo Behrmann <timo.behrmann@gmail.com>
2015-04-11 18:30:15 +0200

    before twit

--- index.js
+++ index.js
@@ -1,71 +1,110 @@
-var Twitter = require('twitter')
+var winston = require('winston');
+var logger = new (winston.Logger)({ transports: [ new (winston.transports.Console)({'timestamp':true}) ]});
+
 var _ = require('lodash')
 var Push = require('pushover-notifications')
 var config = require('./config.json')
-var client = new Twitter(config.twitter)
+
+var twitterConfig = config.twitter;
+var Twitter = require('twitter')
+var client = new Twitter(twitterConfig)
+
 var p = new Push(config.pushover)
 
 var follow = config.follow
-var params = {follow: follow.join(',')}
-
-var cleanup = function (text) {
-  if (typeof text === 'string') {
-    return text.replace(/http:\/\/[^\s#]*/g, '').replace(/#GoalFlash/g, '').replace(/^Correction: /g, 'Korrektur: ').replace(/[\s]{2,50}/g, ' ')
-  }
-  return text
+var params = {
+  follow: follow.join(',')
 }
 
-var post = function (text) {
-  if (!text || config.post !== true) {
-    return
-  }
+var activeStream;
 
-  client.post('statuses/update', {status: text}, function (error, tweet, response) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log('Tweet posted: ' + text)
+var internals = {
+  cleanup: function(text) {
+    if (typeof text === 'string') {
+      return text.replace(/http:\/\/[^\s#]*/g, '').replace(/#GoalFlash/g, '').replace(/^Correction: /g, 'Korrektur: ').replace(/[\s]{2,50}/g, ' ')
     }
-  })
-}
-
-var notify = function (text) {
-  if (!text || config.push !== true) {
-    return
-  }
-
-  var title = 'Pushscore'
-
-  if (text.indexOf('#Bundesliga') !== -1) {
-    title = 'Bundesliga'
-  } else if (text.indexOf('#ChampionsLeague') !== -1) {
-    title = 'Champions League'
-  }
-
-  p.send({ message: text, title: title }, function (err, result) {
-    if (err) {
-      throw err
+    return text
+  },
+  post: function(text) {
+    if (!text || config.post !== true) {
+      return
     }
 
-    console.log('Notification sent: ' + text, result)
-  })
-}
+    client.post('statuses/update', {
+      status: text
+    }, function(error, tweet, response) {
+      if (error) {
+        logger.log('error', 'Tweet failed', error)
+      } else {
+        logger.log('info', 'Tweet posted: ' + text)
+      }
+    })
+  },
+  notify: function(text) {
+    if (!text || config.push !== true) {
+      return
+    }
 
-client.stream('statuses/filter', params, function (stream) {
-  post(config.message)
+    var title = 'Pushscore'
 
-  stream.on('data', function (tweet) {
+    if (text.indexOf('#Bundesliga') !== -1) {
+      title = 'Bundesliga'
+    } else if (text.indexOf('#ChampionsLeague') !== -1) {
+      title = 'Champions League'
+    }
+
+    p.send({
+      message: text,
+      title: title
+    }, function(err, result) {
+      if (err) {
+        throw err
+      }
+
+      logger.log('info', 'Push message sent: ' + text, result)
+    })
+  },
+  onTweet: function(tweet) {
     if (!tweet) {
       return
     } else if (_.contains(follow, tweet.user.id)) {
-      console.log('Pushing updated', tweet.user.name, tweet.user.id, tweet.text)
-      var message = cleanup(tweet.text)
-      notify(message)
-      post(message)
+      logger.log('info', 'Pushing updated', tweet.user.name, tweet.user.id, tweet.text)
+      var message = internals.cleanup(tweet.text)
+      internals.notify(message)
+      internals.post(message)
     }
-  })
-
-  stream.on('error', function (error) {
+  },
+  onError: function(error) {
     throw error
-  })
-})
+  },
+  onEnd: function() {
+    logger.log('info', 'stream end', arguments)
+  },
+  start: function() {
+    client.stream('statuses/filter', params, function(stream) {
+      activeStream = stream
+      internals.post(config.message)
+      stream.on('data', internals.onTweet)
+      stream.on('error', internals.onError)
+      stream.on('end', internals.onEnd)
+    })
+  },
+  restart: function() {
+    logger.log('info', 'Restarting stream...')
+
+    if (activeStream) {
+      try {
+        activeStream.destroy();
+      } catch (e) {
+        logger.log('error', 'Destroying stream failed', e);
+      }
+    }
+
+    internals.start();
+  }
+}
+
+logger.log('info', 'Starting pushover')
+internals.start();
+
+module.exports = internals;
